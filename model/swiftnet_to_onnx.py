35dbcfe2975697dea4b5b7f08229167d971c9c14
Stjepan <mocilac.stjepan@gmail.com>
2019-06-23 18:04:15 +0200

    parametrized input resolution

--- model/swiftnet_to_onnx.py
+++ model/swiftnet_to_onnx.py
@@ -6,13 +6,15 @@ from models.resnet.resnet_single_scale import *
 from models.semseg import SemsegModel
 import numpy as np
 
+InResolution = [1,3,1024,2048]
+
 if __name__=='__main__':
     use_bn = True
     resnet = resnet18(pretrained=False, efficient=False, use_bn=use_bn)
     model = SemsegModel(resnet, 19, use_bn=use_bn)
     model.load_state_dict(torch.load('/home/smocilac/dipl_seminar/swiftnet/weights/swiftnet_ss_cs.pt'), strict=True)
     model.to('cuda') 
-    input_ = torch.ones([1,3,256,512]).to('cuda')
-    model.forward(input_)
-    #torch.onnx.export(model, input_, "swiftnet.onnx", verbose=True, do_constant_folding=True)
+    input_ = torch.ones(InResolution).to('cuda')
+    #model.forward(input_)
+    torch.onnx.export(model, input_, "swiftnet.onnx", verbose=True, do_constant_folding=True)
 
